914835ba1fdc188a36d076869b0db6ea79709ee4
PorridgeSwim <yz3883@columbia.edu>
2024-01-18 16:50:21 -0800

    implement mongodb database

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
@@ -19,13 +20,22 @@ router.post(
     ],
     async (req: Request, res: Response) => {
         const errors = validationResult(req);
+
         if (!errors.isEmpty()) {
             throw new RequestValidationError(errors.array());
         }
+
         const { email, password } = req.body;
-        console.log('Creating a user...');
-        throw new DatabaseConnectionError();
-        res.send({});
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            throw new BadRequestError('Email in use');
+        }
+
+        const user = User.build({ email, password });
+        await user.save();
+        res.status(201).send(user);
     }
 );
 
